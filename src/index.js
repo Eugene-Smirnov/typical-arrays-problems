a33aa42f4e323a4a7fbbbd49fbcf5f4421ff7ea2
Eugene-Smirnov <SmirnovEuV@gmail.com>
2021-02-13 16:23:57 +0300

    feat: add max, min, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if (!Array.isArray(array)) return 0;
+  let result = array[0]
+  for (let num of array) {
+    if (!num && num !== 0) return 0; 
+    if (result > num) result = num;
+  };
+  return (result == undefined) ? 0 : result;
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+  if (!Array.isArray(array)) return 0;
+  let result = array[0];
+  for (let num of array) {
+    if (!num && num !== 0) return 0; 
+    if (result < num) result = num;
+  };
+  return (result == undefined) ? 0 : result;
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if (!Array.isArray(array)) return 0;
+  let result = 0;
+  for (let num of array) {
+    if (!num && num !== 0) return 0; 
+    result += num;
+  };
+  result /= (array.length);
+  return (!result) ? 0 : result;
+};
